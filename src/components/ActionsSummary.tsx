a7f5d02160bb1acd3813d7638fcb6b4ced228076
Edmund <matrim.eg@gmail.com>
2025-09-23 23:47:38 +0200

    Fixed build

--- src/components/ActionsSummary.tsx
+++ src/components/ActionsSummary.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Share, Copy, Check } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import {
